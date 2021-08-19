6bccf480716217c9c95dce3de182ae14ad418aae
Tim Restieaux <restieauxbro@hotmail.com>
2021-08-19 17:11:42 +1200

    add netlify cms

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,19 +1,19 @@
 import React from "react"
-import {motion, AnimatePresence} from 'framer-motion'
+import { motion } from "framer-motion"
 import Layout from "../components/layout/layout"
 
 const Contact = () => {
   return (
     <Layout>
       <h1>Hello Contact</h1>
-      
+
       <motion.div
         style={{ width: 560, height: 300 }}
         className="box"
         layoutId="box"
-        animate={{backgroundColor: "#fafafa"}}
+        animate={{ backgroundColor: "#fafafa" }}
       />
-      
+           <div></div>
     </Layout>
   )
 }
